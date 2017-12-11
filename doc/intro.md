0280c99bbbe695db7e2d9fe0ce6b0627410ca671
Javed Mohamed <jmohamed001@gmail.com>
2017-12-10 18:22:00 -0600

    Create lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cs125attendance
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cs125attendance/core_test.clj
@@ -0,0 +1,7 @@
+(ns cs125attendance.core-test
+  (:require [clojure.test :refer :all]
+            [cs125attendance.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
