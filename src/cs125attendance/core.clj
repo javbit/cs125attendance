4d8b5fc0b1a20c6f1329ccabeafd7e7b96781528
Javed Mohamed <jmohamed001@gmail.com>
2017-12-10 19:24:53 -0600

    Add test for pair as well as its method stub.

--- src/cs125attendance/core.clj
+++ src/cs125attendance/core.clj
@@ -5,3 +5,9 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn pair
+  "Make sequential pairs from a sequence."
+  [sequence]
+  ;; TODO: implement this function.
+  nil)
--- test/cs125attendance/core_test.clj
+++ test/cs125attendance/core_test.clj
@@ -2,6 +2,27 @@
   (:require [clojure.test :refer :all]
             [cs125attendance.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; Pairing should take a sequence and make as many sequential tuples as
+;; possible. Let S be a sequence of length n. If n is even, then (pair S) should
+;; return a sequence of length n/2 containing sequential tuples. If n is 1, then
+;; return the single element. For any odd n greater than 1, there should be one
+;; triple and the rest tuples.
+(deftest test-pair
+  (testing "Making pairs from"
+    (testing "an empty sequence"
+      (is (= '()
+             (pair nil)))
+      (is (= '()
+             (pair '()))))
+    (testing "an even-length sequence"
+      (is (= '((:a :b))
+             (pair '(:a :b))))
+      (is (= '((:a :b) (:c :d))
+             (pair '(:a :b :c :d)))))
+    (testing "an odd-length sequence"
+      (is (= '((:a))
+             (pair '(:a))))
+      (is (= '((:a :b :c))
+             (pair '(:a :b :c))))
+      (is (= '((:a :b) (:c :d :e))
+             (pair '(:a :b :c :d :e)))))))
